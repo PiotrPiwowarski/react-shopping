e7ffa450a9ae8f9ace0461f9449a98018024b22e
Piotr Piwowarski <piotr@piwowarski-uniet.com>
2025-01-12 17:22:59 +0100

    new commit

--- public/service-worker.js
+++ public/service-worker.js
@@ -66,7 +66,7 @@ self.addEventListener("install", (event) => {
       caches
         .open("shopply-pwa-cache")
         .then((cache) =>
-          cache.addAll(["/", "/index.html", "/icon192x192.png", "//icon512x512.png", "/manifest.json"])
+          cache.addAll(["/", "/icon192x192.png", "/icon512x512.png", "/manifest.json"])
         )
     );
   });
